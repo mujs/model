bce67a9afceab62efcc33294da8307cfab52a2d5
Pau Fracés <pfraces@gmail.com>
2015-04-26 19:31:36 +0200

    Initial release
    *   model.object
    *   model.array

--- model.js
+++ model.js
@@ -1,15 +1,14 @@
-define('model', function (require) {
+define('model.object', function (require) {
   'use strict';
 
   var isDefined  = require('mu.is.defined'),
       isFunction = require('mu.is.function'),
       partial    = require('mu.fn.partial'),
       map        = require('mu.list.reduce'),
-      emitter    = require('mu.api.emitter');
+      events     = require('async.events');
 
   var getterSetter = function (channel, model, attr, newVal) {
     if (isDefined(newVal)) {
-      channel.emit('change', attr, newVal, model[attr]);
       channel.emit(attr, newVal, model[attr]);
       model[attr] = newVal;
     }
@@ -17,16 +16,108 @@ define('model', function (require) {
     return model[attr];
   };
 
-  var model = function (defaults) {
-    var channel = emitter();
+  var object = function (config) {
+    var channel = events();
 
-    var model = map(defaults, function (item, index) {
-      if (isFunction(item)) { return item; };
-      return partial(getterSetter, channel, defaults, index);
+    var model = function () {
+      return map(config, function (item) {
+        if (isFunction(item)) { return item(model); };
+        return item;
+      });
+    };
+
+    each(config, function (item, index) {
+      model[index] = (isFunction(item)
+        ? partial(item, model)
+        : partial(getterSetter, channel, config, index)
+      );
     });
 
     model.on = channel.on;
+    model.emit = channel.emit;
   };
 
-  return model;
+  return object;
+});
+
+define('model.array', function (require) {
+  'use strict';
+
+  var partial = require('mu.fn.partial'),
+      each    = require('mu.list.each'),
+      map     = require('mu.list.reduce'),
+      remove  = require('mu.list.remove'),
+      indexOf = require('mu.list.indexOf'),
+      events  = require('async.events');
+
+  var identity = function (arg) {
+    return arg;
+  };
+
+  var mixin = function (target) {
+    return function (item, index) {
+      target[index] = item;
+    };
+  };
+
+  var array = function (config) {
+    var data = [],
+        channel = events();
+
+    var model = function () {
+      return map(data, function (item) {
+        if (isFunction(item)) { return item(model); };
+        return item;
+      });
+    };
+
+    each(config, function (item, index) {
+      model[index] = partial(item, model);
+    });
+
+    model.change = function (item, newVal) {
+      var index = indexOf(data, item);
+      data[index] = newVal;
+      channel.emit('change', newVal, item);
+    };
+
+    model.update = function (item, newVal) {
+      model.change(item, each(newVal, mixin(item)));
+    };
+
+    model.remove = function (item) {
+      remove(data, item);
+      channel.emit('remove', item);
+    };
+
+    model.item = function (item) {
+      return {
+        value: partial(identity, item),
+        change: partial(model.change, item),
+        update: partial(model.update, item),
+        remove: partial(model.remove, item)
+      };
+    };
+
+    model.insert = function (item) {
+      data.push(item);
+      channel.emit('insert', model.item(item));
+    };
+
+    model.on = channel.on;
+    model.emit = channel.emit;
+
+    return model;
+  };
+
+  return array;
+});
+
+define('model', function (require) {
+  'use strict';
+
+  return {
+    model : require('model.object'),
+    array : require('model.array')
+  };
 });
